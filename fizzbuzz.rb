410c9ceaeb2482c85f9ba161ecf13a4dbf38bd03
garvin tan <garvintan07@gmail.com>
2019-05-30 01:12:07 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,10 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(n)
+  return "Fizzbuzz" if n % 3 == 0 && n % 5 == 0
   return "Fizz" if n % 3 == 0 
   return "Buzz" if n % 5 == 0
-  return "Fizzbuzz" if n % 3 == 0 && n % 5 == 0 
+   
   nil 
 
 end
